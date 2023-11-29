b472b74f1623111423979485364581efc261b761
AndersonMolano <anderson27mosam@gmail.com>
2023-11-28 21:08:23 -0500

    Panel de admin

--- rest/Citas/listar_citas.php
+++ rest/Citas/listar_citas.php
@@ -7,10 +7,13 @@ $dbConn = connect($db);
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     try {
         // Consulta todas las citas con información de usuario y servicio
-        $sql = $dbConn->prepare("SELECT cita.*, usuario.name_user as nombreUsuario, servicio.type_servi as nombreServicio
+        $sql = $dbConn->prepare("
+            SELECT cita.*, usuario.name_user as nombreUsuario, servicio.type_servi as nombreServicio
             FROM cita
-            LEFT JOIN usuario ON cita.idClieFK = usuario.id_user
-            LEFT JOIN servicio ON cita.idServiFK = servicio.id_servi");
+            LEFT JOIN cliente ON cita.idClieFK = cliente.id_client
+            LEFT JOIN usuario ON cliente.idUserFk = usuario.id_user
+            LEFT JOIN servicio ON cita.idServiFK = servicio.id_servi
+        ");
 
         $sql->execute();
         $citas = $sql->fetchAll(PDO::FETCH_ASSOC);
